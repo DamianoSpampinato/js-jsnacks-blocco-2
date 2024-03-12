21f4cb09c393bbd4743ff49726b36d9968c76f78
DamianoSpampinato <ghgg585@gmail.com>
2024-03-12 16:27:30 +0100

    feat: add const +init for cycle

---
+++ snack-3/js/script.js
@@ -0,0 +1,12 @@
+//- Crea due tag div con due id diversi:
+//un div avrà il testo colorato di rosso mentre l’altro di verde.
+//Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const oddArray = [];
+const evenArray =[];
+for(let i= 0; i < numberArray.length; i++){
+let thisNumber = numberArray[i];
+   
+
+
+}
