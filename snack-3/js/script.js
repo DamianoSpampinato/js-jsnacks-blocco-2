6e003b1581f733e8cabf9404ffd79fbfed8a6aeb
DamianoSpampinato <ghgg585@gmail.com>
2024-03-12 16:38:39 +0100

    feat: add if and else to cycle for,

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -2,11 +2,18 @@
 //un div avrà il testo colorato di rosso mentre l’altro di verde.
 //Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const oddArray = [];
-const evenArray =[];
+const redDiv = document.querySelector('#container-red');
+const greenDiv = document.querySelector('#green');
 for(let i= 0; i < numberArray.length; i++){
 let thisNumber = numberArray[i];
-   
+    if(thisNumber % 2 === 0){
+    greenDiv.innerHTML += (thisNumber);
+
+    
+    } else{
+    redDiv.innerHTML += (thisNumber);
+
+    }
 
 
 }
